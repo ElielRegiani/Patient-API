6c77a880bf63b850e59d8a32ebadecd6bb599457
ElielRegiani <elielregiani@gmail.com>
2024-02-08 15:25:49 -0300

    First commit

---
+++ api/src/test/java/pac/voll/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pac.voll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
